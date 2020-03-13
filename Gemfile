2f4b1ee51589e7f5dc9e57b1841adc2344da2943
Jason Hutchens <jasonhutchens@gmail.com>
2020-03-13 14:56:44 +0800

    flesh out regions

--- Gemfile
+++ Gemfile
@@ -4,3 +4,4 @@ source 'https://rubygems.org'
 ruby "2.7.0"
 
 gem 'byebug', '~> 11.1.1'
+gem 'countries', '~> 3.0.1'
--- corona.rb
+++ corona.rb
@@ -5,6 +5,27 @@ require 'csv'
 require 'json'
 require 'byebug'
 
+require 'countries'
+
+region_map = JSON.parse(File.read('region_map.json'))
+region_map.each do |name, value|
+  next if value.is_a?(Hash)
+  puts name
+  country = ISO3166::Country.find_country_by_name(name)
+  country ||= ISO3166::Country.find_country_by_name(value)
+  next if country.nil?
+  region_map[name] = {
+    id: country.alpha2,
+    name: country.name,
+    region: country.region,
+    subregion: country.subregion,
+    flag: country.emoji_flag
+  }
+end
+File.write('region_map.json', JSON.pretty_generate(region_map))
+
+exit
+
 root = ARGV[0] || ENV.fetch('COVID19_PATH', nil) || File.join('..', 'COVID-19')
 path = File.join(root, 'csse_covid_19_data', 'csse_covid_19_daily_reports')
 raise "No such directory: #{path}" unless Dir.exist?(path)
