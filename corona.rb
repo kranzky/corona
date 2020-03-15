8fd1c2b804512e71d0bbf8f3c6bac471f7e93e1f
Jason Hutchens <jasonhutchens@gmail.com>
2020-03-16 00:10:19 +0800

    better growth

--- corona.rb
+++ corona.rb
@@ -243,15 +243,15 @@ def generate_metadata(data)
   end
   return if data[:series].nil?
   prev = {
-    confirmed: [0,0,0,0],
-    deaths: [0,0,0,0],
-    recovered: [0,0,0,0]
+    confirmed: [0,0],
+    deaths: [0,0],
+    recovered: [0,0]
   }
   data[:series].each do |key, value|
     [:confirmed, :deaths, :recovered].each do |status|
       total = value[status][:total]
-      prev_delta = prev[status][2] - prev[status][0]
-      this_delta = total - prev[status][2]
+      prev_delta = prev[status][1] - prev[status][0]
+      this_delta = total - prev[status][1]
       value[status][:growth] = this_delta.to_f / prev_delta.to_f if prev_delta != 0
       value[status][:delta] = total - prev[status].last
       prev[status] << total
